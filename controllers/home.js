8ba04a0fd344da89a276b603487f202d74e3708a
Suraksha <suraksha@emingli360.com>
2020-05-05 17:38:05 +0800

    mongodb aggregrate function is added

--- controllers/home.js
+++ controllers/home.js
@@ -14,10 +14,26 @@ module.exports=function(async,Club,_,){
                     Club.find({}, (err, result) => {
                         callback(err, result);
                     });
-                },
+				},
+				function(callback){
+					Club.aggregate([
+						{ $group: { _id:"$country" } },
+						
+					  ],(err,newResult)=>{
+						  callback(err,newResult);
+					  });
+					  }
 			],(err,results)=>{
 				const res1=results[0];
-				res.render('home',{title:"Footballlkik-Home",data:res1})
+				const res2=results[1];
+				console.log(res2);
+				const dataChunk=[];
+				const chunkSize=3;
+				for(let i=0; i<res1.length;i+=chunkSize){
+					dataChunk.push(res1.slice(i,i+chunkSize));
+				}
+				const countrySort = _.sortBy(res2,'_id')
+				res.render('home',{title:"Footballlkik-Home",data:dataChunk,country:countrySort})
 			})   
 		}
 	}    
